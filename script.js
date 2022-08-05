522e5a3d45df8ba2e1d13dea0a58295052b31842
zanoniFab <fabianezanoni@gmail.com>
2022-08-05 14:53:29 -0300

    var to object

--- script.js
+++ script.js
@@ -1,63 +1,55 @@
-var listaImagens = ["https://upload.wikimedia.org/wikipedia/pt/3/32/The_Notebook_p%C3%B4ster.jpg","https://images-na.ssl-images-amazon.com/images/I/61MeTlsDRtL.jpg"];
+var filmes = [];
 
-var listaNomes = ["Diário de Uma Paixão","A Lista de Schindler"];
-
-var nomesTratados = ["diariodeumapaixao","alistadeschindler"];
-
-mostrarFilmes();
-const inputSearch = document.querySelector("#search");
-
-inputSearch.addEventListener("input", function(){
-  const str = this.value;
-  console.log(str)
-})
-
-
-
-
-
-
-function adicionarFilme() {
-  let imagem = document.getElementById("imagemFilme").value;
-  let nome = document.getElementById("nomeFilme").value;
-  let newName = tratarNome(nome);
-  verifyExists(imagem,nome,newName);
+function verificarRepetido(id){
+  for (i=0; i<filmes.length;i++){
+    if (filmes[i].id == id){
+      return true;
+    }
+  }
+  return false;
 }
-function verifyExists(imagem,nome,newName){
-  if (nomesTratados.includes(newName,0)=== true){
+function criarObjeto(){ //faz uma lista com os filmes já em formato de objeto
+  let novoFilmeNome = document.getElementById("nomeFilme").value;
+  let novoFilmeImagem = document.getElementById("imagemFilme").value;
+  let id = tratarNome(novoFilmeNome);
+  if (!verificarRepetido(id)) {
+      if (novoFilmeImagem.endsWith(".jpg")){
+        let novoFilme = {
+          nome: novoFilmeNome,
+          imagem: novoFilmeImagem,
+          id: id,
+        }
+        filmes.push(novoFilme);
+        mostrarFilmes();
+      } else {
+        alert("Somente imagens no formato jpg são aceitas");
+      }
+  } else {
     alert("Esse filme já existe!");
-  } else {
-    verifyJpg(imagem,nome,newName);
   }
 }
-function verifyJpg(imagem,nome,newName){
-  if (imagem.endsWith(".jpg")){
-    listaImagens.push(imagem);
-    listaNomes.push(nome);
-    nomesTratados.push(newName);
-    mostrarFilmes();
-  } else {
-    alert("Somente imagens no formato jpg são aceitas");
-  }
+function tratarNome(nome) {
+  return nome.normalize('NFD').replace(/[\u0300-\u036f]/g, '') //acento
+    .toLowerCase() //letra minuscula
+    .replace(/\s+/g,""); //espaco em branco
 }
 function mostrarFilmes(){
   limpaTela();
-  let eListaFilmes = document.getElementById("listaFilmes");
-  for (let i=0; i<listaNomes.length; i++) {
-    let nomeFilme = listaNomes[i];
-    let nomeTratado = nomesTratados[i];
-    let img = "<img src ='"+ listaImagens[i] +"'>";
-    let p = "<p class='nomeFilme' style='visibility:hidden'>"+listaNomes[i]+"</p>";
+  let divFilmes = document.getElementById("listaFilmes");
+  for (let i=0; i<filmes.length; i++) {
+    let nome = filmes[i].nome;
+    let id = filmes[i].id;
+    let imagem = "<img src ='"+ filmes[i].imagem +"'>";
+    let p = "<p class='nomeFilme' style='visibility:hidden'>"+filmes[i].nome+"</p>";
     //let button = "<button class='lixo' style='visibility:hidden'".concat("onClick=excluirFilme(", "\"", nome_filme, "\"", ")>❌</button>;")
-    let button = "<button class='lixo' style='visibility:hidden' onClick='excluirFilme(\""+ nomeFilme + "\")'>❌</button>";
-    let div = "<div id = '"+nomesTratados[i]+"' onmouseover='showOptions()'  onmouseout='hideOptions()' class='divFilme'>" + img + p + button +"</div>";
-    eListaFilmes.innerHTML+=  div;
+    let button = "<button class='lixo' style='visibility:hidden' onClick='excluirFilme(\""+ id + "\")'>❌</button>";
+    let div = "<div id = '"+filmes[i].id+"' onmouseover='showOptions()'  onmouseout='hideOptions()' class='divFilme'>" + imagem + p + button +"</div>";
+    divFilmes.innerHTML+=  div;
   }
   document.getElementById("imagemFilme").value="";
   document.getElementById("nomeFilme").value="";
   document.getElementById("search").value="";
 }
-
 function limpaTela(){
   const divFilmes = document.getElementById("listaFilmes");
   divFilmes.innerHTML = "";
@@ -72,20 +64,11 @@ function hideOptions(){
   target.querySelector(".lixo").style.visibility="hidden"
   target.querySelector(".nomeFilme").style.visibility="hidden";
 }
-
-function excluirFilme(nome) {
-  let excluirNew = tratarNome(nome)
-  for (let i=0; i<this.listaNomes.length; i++){
-    if (this.listaNomes[i] == nome){
-        listaNomes.splice(i, 1);
-        listaImagens.splice(i, 1);
-        nomesTratados.splice(i,1);
-      }
+function excluirFilme(id) {
+  for (let i=0; i<this.filmes.length; i++){
+    if (this.filmes[i].id == id){
+        filmes.splice(i, 1);
+    }
     mostrarFilmes();
-}
-}
-function tratarNome(nome) {
-  return nome.normalize('NFD').replace(/[\u0300-\u036f]/g, '') //acento
-    .toLowerCase() //letra minuscula
-    .replace(/\s+/g,""); //espaco em branco
+  }
 }
